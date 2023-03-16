1bf9e71c74e8aa76202e2ff69854ffc8cce34310
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-03-16 10:55:04 -0500

    post.get by id is done and tests are passing.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -20,6 +20,25 @@ router.get('/', async(req, res) => {
   })
 })
 
+router.get('/:id', async(req, res) => {
+  const { id } = req.params
+  Post.findById(id)
+  .then(post => {
+    if(!post) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist" 
+      })
+    }
+    res.json(post)
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: "The post information could not be retrieved",
+      err: err.message,
+      stack: err.stack,
+    })
+  })
+})
 
 
 module.exports = router
