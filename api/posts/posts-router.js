7d78853e714e9183e892d06e87f2e4191dbd62fd
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-03-16 11:21:34 -0500

    post.post is done and tests are passing.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -41,4 +41,28 @@ router.get('/:id', async(req, res) => {
 })
 
 
+router.post('/', async(req, res) => {
+  const post = req.body
+  if(!post.contents || !post.title) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post"
+    })
+  } else {
+    Post.insert(post)
+    .then(({ id }) => {
+      return Post.findById(id)
+    })
+    .then(newPost => {
+      res.status(201).json(newPost)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was an error while saving the post to the database",
+        err: err.message,
+        stack: err.stack,
+      })
+    })
+  }
+})
+
 module.exports = router
