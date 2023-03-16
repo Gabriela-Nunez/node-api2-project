85936cb93d116dcf6a84de4e6b63bb513798276d
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-03-15 21:58:49 -0500

    Connected index.js, to server.js and server.js to post-router.js

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,23 @@
 // implement your posts router here
+const express = require('express')
+const Post = require('./posts-model')
+
+const router = express.Router()
+
+//ENDPOINTS
+
+router.get('/', async (req, res) => {
+  Post.find()
+  .then(posts => {
+    res.json(posts)
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: "The posts information could not be retrieved",
+      err: err.message,
+      stack: err.stack
+    })
+  })
+})
+
+module.exports = router
