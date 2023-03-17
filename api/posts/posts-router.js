90fa1b91bdbda68e17439887cac93c9eb84aed96
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-03-16 19:52:27 -0500

    post.put is done and test are passing.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -40,7 +40,6 @@ router.get('/:id', async(req, res) => {
   })
 })
 
-
 router.post('/', async(req, res) => {
   const post = req.body
   if(!post.contents || !post.title) {
@@ -63,6 +62,40 @@ router.post('/', async(req, res) => {
       })
     })
   }
+})
+
+router.put('/:id', (req, res) => {
+  const { contents, title } = req.body
+  if(!contents || !title) {
+    res.status(400).json({
+     message: "Please provide title and contents for the post"
+    })
+  } else {
+    Post.findById(req.params.id)
+    .then(possiblePost => {
+      if(!possiblePost) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist", 
+        })
+      } else {
+        return Post.update(req.params.id, req.body)
+      }
+    })
+    .then(data => {
+      if(data) {
+        return Post.findById(req.params.id)
+      }
+    })
+    .then(post => {
+      res.json(post)
+    }).catch(err => {
+      res.status(500).json({
+        message: "The post information could not be modified",
+        err: err.message,
+        stack: err.stack,
+      })
+    })
+  } 
 })
 
 module.exports = router
